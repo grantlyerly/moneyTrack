66054ccaa3942e949c4e17db53a2089714a0d980
Grant Lyerly <grant.e.lyerly@gmail.com>
2023-12-06 21:20:23 -0500

    functionality to party supported

--- contributor_data.py
+++ contributor_data.py
@@ -8,7 +8,7 @@ https://www.opensecrets.org/donor-lookup/results?name=buddy+bengel&order=desc&so
 https://www.opensecrets.org/donor-lookup/results? name = NAME &order=desc & sort=D & zip = ZIP
 """
 
-def collect(name, zipCode):
+def collect_contributions(name, zipCode):
     namePlus = name.replace('_', '+')
 
     url = f"https://www.opensecrets.org/donor-lookup/results?name={namePlus}&order=desc&sorD&zip={zipCode}"
@@ -19,35 +19,82 @@ def collect(name, zipCode):
     body = soup.body
     
     data = body.find_all('tr')
-    str_data = []
 
-    for a in data:
+    ret=[]
+
+    for a in data[1:]:
         item = str(a)
         badstr = ['<tr>', '<td class="category orange">', '<td>', 
                   '</td>', '/tr', '<td class="u-nowrap">', '<br/>',
-                  '\t', '<>']
+                  '\t', '<>', '<td class="category green">', '<td class="c-red u-center" colspan="8" style="font-size: 12px;"><b>FEDERAL LAW PROHIBITS THE USE OF CONTRIBUTOR INFORMATION FOR THE PURPOSE OF SOLICITING CONTRIBUTIONS OR FOR ANY COMMERCIAL PURPOSE.</b>']
         for char in badstr:
             item = item.replace(char, "")
-        
-        new_str = str()
-        for index in range(len(item)-1):
-            if item[index] != '\n':
-                new_str += item[index]
-            else:
-                if index != 0:
-                    if item[index-1] != '\n':
-                        new_str += ','
 
-        new_item = new_str.split(',')
-        str_data.append(new_item)
+        splitItem = item.split('\n')
+
+        """
+        Set up as follows:
+
+        \n
+        \n
+        money to candidates
+        \n
+        \n
+        name
+        city, zip
+        \n
+        employer
+        occupation
+        date
+        money
+        recipient
+        state
+        \n
+        """
+
+        if len(splitItem) > 5:
+            split_location = splitItem[6].split(',')
+
+            results = {'category':splitItem[2], 'contributor': splitItem[5], 'city': split_location[0], 'zipCode': split_location[1].strip(' '), 
+                   'employer': splitItem[8], 'occupation': splitItem[9], 'data': splitItem[10], 'amount':splitItem[11], 'recipient':splitItem[12], 'jurisdiction':splitItem[13]}
+            
+            ret.append(results)
+
+    return(ret)
 
 
-    test = str_data[1]
+def parse_contributions(contribution):
+    amount_str = contribution['amount'].strip('$')
+    amount = int(amount_str.replace(',', ''))
+    target = contribution['recipient']
+    target_party = target[-2]
 
-    print(type(str_data))
-    print(type(test))
+    return amount, target_party
 
-    print(str_data)
+
+def impact(name, zipCode):
+    contrs = collect_contributions(name, zipCode)
+    r = 0
+    d = 0
+    for contr in contrs:
+        amount, tp = parse_contributions(contr)
+        if tp == 'R':
+            r += amount
+        if tp == 'D':
+            d += amount
+        print(amount, tp)
+    
+    print("Republican = " + str(r))
+    print("Democrat = " + str(d))
+
+    perc = max((r/(r+d)), (d/(r+d)))
+    party = 'R'
+    if d > r:
+        party = 'D'
+    return(perc, party)
+
 
 if __name__ == '__main__':
-    collect('buddy bengel', 28562)
+    # name = input("name: ")
+    # zipCode = input('zipCode: ')
+    print(impact('buddy bengel', 28562))
